8bf6d3effd0abff50bbbb8ef78414bf0fc229aa4
j-daniel <jdaniel7@stanford.edu>
2018-11-15 12:01:07 -0800

    revised samplenextstate

--- baseline.py
+++ baseline.py
@@ -41,40 +41,29 @@ def getActions(state):
 			all_actions.append(action)
 	return all_actions
 
-def getStates(state, action):
+def sampleNextStateReward(state, action):
 
 	# TODO alter per-country resistances to reflect new resource allocation based on action. diminishing returns?
 	# use y = x / (1+x) along w action
 
-	nextStates = {tuple(state) : state}
-	nextProbs = {tuple(state) : 0}
+
+	newState = state[:]
 
 	for index in range(NUM_COUNTRIES):
-		for key, newState in nextStates.items():
-			if newState[index] == 0:
-				p = 0.1           # TODO dummy value. need info about neighbors - lookup dict?
-				newStateFlip = newState[:index] + [1] + newState[index + 1:]
-				nextStates[tuple(newStateFlip)] = newStateFlip
-				nextProbs[tuple(newStateFlip)] = p
-				nextProbs[tuple(newState)] += (1 - p) # add likelihood of not flipping
-			elif newState[index] == 1:
-				q = newState[index + NUM_COUNTRIES]  # get resistance score = prob(cure) = q
-													 # TODO make this more sophisticated?
-				newStateFlip = newState[:index] + [0] + newState[index + 1:]
-				nextStates[tuple(newStateFlip)] = newStateFlip
-				nextProbs[tuple(newStateFlip)] = q
-				nextProbs[tuple(newState)] += (1 - q) # add likelihood of not flipping
-			else:
-				print 'INFECTION FLAG NON-BINARY VALUE ERROR FOR COUNTRY AT INDEX', index
+		if newState[index] == 0:
+			p = 0.5           # TODO dummy value. need info about neighbors - lookup dict?
+			if random.uniform(0,1) < p:
+				newState = newState[:index] + [1] + newState[index + 1:]
+		elif newState[index] == 1:
+			q = newState[index + NUM_COUNTRIES]  # get resistance score = prob(cure) = q
+												 # TODO make this more sophisticated?
+			if random.uniform(0,1) < q:
+				newState = newState[:index] + [0] + newState[index + 1:]
+		else:
+			print 'INFECTION FLAG NON-BINARY VALUE ERROR FOR COUNTRY AT INDEX', index
 
-	# all probabilities of states should sum to 1, plus need to zip dicts together
-	probSum = 0.0
-	for key, prob in nextProbs.items():
-		probSum += prob
-	nextStatesList = []
-	for key, state in nextStates.items():
-		nextStatesList += [(state, nextProbs[key] / probSum)]
-	return nextStatesList
+	reward = getReward(newState)
+	return newState, reward
 
 # Takes a number and makes it fall between 1 and 0. 
 def normalize(num):
@@ -88,10 +77,8 @@ def getReward(state):
 		if state[i] == 0: num_zeros += 1
 	return num_zeros + leftover
 
-# TODO some require numpy and some require not-numpy oops
-state = np.array([0,1,0,1,0,0,0,0,3])
+state = [0,1,0,1,0,0,0,0,3]
 print getActions(state)
 print(getReward(state))
-stateNotNP = [0,1,0,1,0,0,0,0,3]
-print getStates(stateNotNP, [0,0,0,0])
+print sampleNextStateReward(state, [0,0,0,0])
 
