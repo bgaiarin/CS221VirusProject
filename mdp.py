7d274055f7b2ff466272c6d531929b175d741109
bgaiarin <bgaiarin@stanford.edu>
2018-11-15 16:37:25 -0800

    reorganized files

--- mdp.py
+++ mdp.py
@@ -1,63 +1,143 @@
-'''
-- One function getActions(state) that takes in a state vector (of the
-type [0, 1, 0, 0.6, 0.2, 0.8, 12] we discussed, with data on binary infection,
-resistance score, and remaining resources) and returns a list of action vectors
-(of the type [1, 2, 0] with number of resources to allocate to each country)
-- One function getStates(state, action) that takes in the current state and an
-action and returns the different possible next states and each next state's associated
-	transition probability
----> randomness is introduced here in 2 ways: 1) a country that is infected becomes
-	  un-infected with probability proportional to their resistance score, and
-	  2) a country that is not infected becomes infected with probability
-	  proportional to the amount of flights coming in from neighboring infected
-	  countries
----> side note: a state where every infection flag is 0 is an end state.
-- One function getReward(state) that takes in a state and returns a scalar reward value
-	for that state
-'''
-
+import math, random, csv, itertools
+from collections import defaultdict
 import numpy as np
-NUM_COUNTRIES = 3
 
+responses_csv = 'data/FR_MAUR_NIG_SA_responseIndicators.csv'
+transitions_csv = 'data/FR_MAUR_NIG_SA_transitions.csv'
+NUM_COUNTRIES = 4
 
-def getStates(state, action):
+INDEX_RESOURCE = NUM_COUNTRIES*2
+LEFTOVER_RESOURCES_WEIGHT = 0.6
+INFECTION_COEFFICIENT = 3.0
+PREVENTION_COST = 0.8
+INFECTION_COST = 0.6 # 0 < x < 1, should be <= PREVENTION_COST
+MAX_RESPONSE_SCORE = 0.99
 
-	# TODO alter per-country resistances to reflect new resource allocation based on action. diminishing returns?
-	# use y = x / (1+x) along w action
+def loadFlights(flightdata):
+	countries = []
+	neighbors = {}
+	totalSeats = 0
+	with open(flightdata) as flightFile:
+		next(flightFile)
+		csvReader = csv.reader(flightFile, delimiter=',')
+		for row in csvReader:
+			dest = row[1].strip()
+			if dest not in neighbors.keys():
+				neighbors[dest] = []
+			neighbors[dest] += [(row[0].strip(), int(row[2]))]
+			totalSeats += int(row[2])
+			if dest not in countries:
+				countries.append(dest)
+	countries.append(totalSeats)
+	return countries, neighbors, totalSeats
 
-	nextStates = {tuple(state) : state}
-	nextProbs = {tuple(state) : 0}
+countries, neighbors, totalSeats = loadFlights(transitions_csv)
+print neighbors, totalSeats, countries
 
+# If NUM_COUNTRIES = 4, creates an array of indices: [0, 1, 2, 3]
+def getIndexArray(): 
+	arr = []
+	for i in range(NUM_COUNTRIES):
+		arr.append(i)
+	return arr 
+
+# Get all possible combinations of actions for a given # of resources 
+def iterateGetActions(index_array, resources):
+	combos = itertools.combinations_with_replacement(index_array, resources)
+	arr = []
+	for c in combos: 
+		actions = [0]*NUM_COUNTRIES
+		for index in c: 
+			actions[index] += 1
+		arr.append(actions)
+	return arr
+
+# Given a  state, returns all possible actions (resource allocations). 
+def getActions(state):
+	num_resources = state[INDEX_RESOURCE]
+	all_actions = []
+	index_array = getIndexArray()
+	for i in range(1, num_resources+1):
+		actions = iterateGetActions(index_array, i)
+		for action in actions: 
+			all_actions.append(action)
+	return all_actions
+
+# Takes a number and makes it fall between 1 and 0. 
+def squash(num):
+ 	return (num/(1.0+num))
+
+# Accepts a state and an action (ex: [0, 1, 0, 2, 0]), and updates response indicators for countries 
+# that have been granted additional resources by the action. Returns a state with the updated response indicators.
+# NOTE: There is some funky math here. The equation we use for updating is subject to be changed. 
+# NOTE: Why do we have "scalar += 1"? We add 1 to any resource allocation that's > 0 to ensure that 
+# 		newState[i] gets increased and not decremented. Again, we can choose not to do this if we change
+#		the update equation that we use. 
+def updateResistances(state, action): 
+	newState = state[:]
+	for i in range(NUM_COUNTRIES, INDEX_RESOURCE):
+		scalar = action[i-NUM_COUNTRIES]
+		if (scalar != 0):
+			scalar += 1					
+			update = newState[i]*scalar*(squash(scalar))					
+			newState[i] = ( update if update < 1.0 else MAX_RESPONSE_SCORE )	#Keep in range (0,1)
+	return newState
+
+# gets the probability a country is infected as a function of number of seats coming in from infected neighbors
+def getInfectionProb(index, state, countries, neighbors):
+	country = countries[index]
+	infectedSeats = 0
+	for neighbor in neighbors[country]:
+		if state[countries.index(neighbor[0])] == 1: # if neighbor is infected
+			infectedSeats += neighbor[1]
+	return infectedSeats * INFECTION_COEFFICIENT / countries[-1]
+
+# generates a next state and its reward probabilistically based on current state
+def sampleNextStateReward(state, action, countries, neighbors):
+	newState = state[:]
+
+	# Reduce the resistance scores of infected countries by a coefficient of INFECTION_COST
+	# and uninfected countries by a coefficient of PREVENTION_COST (resources spent in most recent time step).
+	for i in range(NUM_COUNTRIES):
+		if state[i] == 0:
+			newState[NUM_COUNTRIES + i] *= PREVENTION_COST
+		elif state[i] == 1:
+			newState[NUM_COUNTRIES + i] *= INFECTION_COST
+		else:
+			print 'INFECTION FLAG NON-BINARY VALUE ERROR FOR COUNTRY AT INDEX', index
+	print 'after updating scores:',newState
+
+	# Alter per-country resistances in newState to reflect new resource allocation based on action.
+	newState = updateResistances(newState, action)
+	print 'after updating resistances to handle actions:', newState
+
+	# Re-sample a listing of infected and uninfected binary values [0, 1, 0, 0, etc.].
 	for index in range(NUM_COUNTRIES):
-		for key, newState in nextStates.items():
-			if newState[index] == 0:
-				p = 0.1           # TODO dummy value. need info about neighbors - lookup dict?
-				newStateFlip = newState[:index] + [1] + newState[index + 1:]
-				nextStates[tuple(newStateFlip)] = newStateFlip
-				nextProbs[tuple(newStateFlip)] = p
-				nextProbs[tuple(newState)] += (1 - p) # add likelihood of not flipping
-			elif newState[index] == 1:
-				q = newState[index + NUM_COUNTRIES]  # get resistance score = prob(cure) = q
-				newStateFlip = newState[:index] + [0] + newState[index + 1:]
-				nextStates[tuple(newStateFlip)] = newStateFlip
-				nextProbs[tuple(newStateFlip)] = q
-				nextProbs[tuple(newState)] += (1 - q) # add likelihood of not flipping
-			else:
-				print 'INFECTION FLAG NON-BINARY VALUE ERROR FOR COUNTRY AT INDEX', index
+		if newState[index] == 0:
+			p = getInfectionProb(index, state, countries, neighbors)    # p(infected from neighbors)
+			if random.uniform(0,1) < p:
+				newState = newState[:index] + [1] + newState[index + 1:]
+		elif newState[index] == 1:
+			q = state[index + NUM_COUNTRIES]  # get resistance score = prob(cure) = q
+			if random.uniform(0,1) < q:
+				newState = newState[:index] + [0] + newState[index + 1:]
+		else:
+			print 'INFECTION FLAG NON-BINARY VALUE ERROR FOR COUNTRY AT INDEX', index
 
-	# all probabilities of states should sum to 1
-	probSum = 0.0
-	for key, prob in nextProbs.items():
-		probSum += prob
-	nextStatesList = []
-	for key, state in nextStates.items():
-		nextStatesList += [(state, nextProbs[key] / probSum)]
-	return nextStatesList
+	reward = getReward(newState)
+	return newState, reward
 
-state = [0, 1, 0, 0.2, 0.4, 0.6, 3]
-newStates = getStates(state, [0, 1, 0])
-print 'printing new states, len = ', len(newStates)
-print newStates
-print 'printed'
-print 'first elem: ', newStates[0]
+# Return reward for a given state, where reward = (# uninfected countries + weight*leftover_resources)
+def getReward(state): 
+	num_zeros = 0
+	leftover = squash(state[INDEX_RESOURCE])*LEFTOVER_RESOURCES_WEIGHT
+	for i in range(0, NUM_COUNTRIES): 
+		if state[i] == 0: num_zeros += 1
+	return num_zeros + leftover
+
+state = [0,1,0,1,0.1,0.3,0.3,0.7,3]
+print state
+print getActions(state)
+print(getReward(state))
+print sampleNextStateReward(state, [0,1,1,0], countries, neighbors)
 
---
+++ old work/(old)mdp.py
@@ -0,0 +1,63 @@
+'''
+- One function getActions(state) that takes in a state vector (of the
+type [0, 1, 0, 0.6, 0.2, 0.8, 12] we discussed, with data on binary infection,
+resistance score, and remaining resources) and returns a list of action vectors
+(of the type [1, 2, 0] with number of resources to allocate to each country)
+- One function getStates(state, action) that takes in the current state and an
+action and returns the different possible next states and each next state's associated
+	transition probability
+---> randomness is introduced here in 2 ways: 1) a country that is infected becomes
+	  un-infected with probability proportional to their resistance score, and
+	  2) a country that is not infected becomes infected with probability
+	  proportional to the amount of flights coming in from neighboring infected
+	  countries
+---> side note: a state where every infection flag is 0 is an end state.
+- One function getReward(state) that takes in a state and returns a scalar reward value
+	for that state
+'''
+
+import numpy as np
+NUM_COUNTRIES = 3
+
+
+def getStates(state, action):
+
+	# TODO alter per-country resistances to reflect new resource allocation based on action. diminishing returns?
+	# use y = x / (1+x) along w action
+
+	nextStates = {tuple(state) : state}
+	nextProbs = {tuple(state) : 0}
+
+	for index in range(NUM_COUNTRIES):
+		for key, newState in nextStates.items():
+			if newState[index] == 0:
+				p = 0.1           # TODO dummy value. need info about neighbors - lookup dict?
+				newStateFlip = newState[:index] + [1] + newState[index + 1:]
+				nextStates[tuple(newStateFlip)] = newStateFlip
+				nextProbs[tuple(newStateFlip)] = p
+				nextProbs[tuple(newState)] += (1 - p) # add likelihood of not flipping
+			elif newState[index] == 1:
+				q = newState[index + NUM_COUNTRIES]  # get resistance score = prob(cure) = q
+				newStateFlip = newState[:index] + [0] + newState[index + 1:]
+				nextStates[tuple(newStateFlip)] = newStateFlip
+				nextProbs[tuple(newStateFlip)] = q
+				nextProbs[tuple(newState)] += (1 - q) # add likelihood of not flipping
+			else:
+				print 'INFECTION FLAG NON-BINARY VALUE ERROR FOR COUNTRY AT INDEX', index
+
+	# all probabilities of states should sum to 1
+	probSum = 0.0
+	for key, prob in nextProbs.items():
+		probSum += prob
+	nextStatesList = []
+	for key, state in nextStates.items():
+		nextStatesList += [(state, nextProbs[key] / probSum)]
+	return nextStatesList
+
+state = [0, 1, 0, 0.2, 0.4, 0.6, 3]
+newStates = getStates(state, [0, 1, 0])
+print 'printing new states, len = ', len(newStates)
+print newStates
+print 'printed'
+print 'first elem: ', newStates[0]
+
