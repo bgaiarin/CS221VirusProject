345d7fd5cd38891de77242ffe906acc28c784b04
bgaiarin <bgaiarin@stanford.edu>
2018-12-10 14:37:37 -0800

    set up two files for funcapprox and policygrad

--- policygrad.py
+++ policygrad.py
@@ -1,7 +1,85 @@
+
+#####################################################################
+
+# The following is code for a Vanilla Policy Gradient model 
+# Retrieved from: https://github.com/awjuliani/DeepRL-Agents/blob/master/Vanilla-Policy.ipynb
+# Original retrieved OS code has been modified to fit our problem. 
+
+# NOTES: 
+# 1. Will need to use our MDP instead of "gym" environment. 
+# 2. Will need to generalize to more than 2 actions. 
+
+#### PACKAGES #################################
+
 from mdp import EpidemicMDP 
 import random 
 from collections import defaultdict
 import math 
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
 import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+try:
+    xrange = xrange
+except:
+    xrange = range
 
 #### GLOBAL VARIABLES #################################
+
+gamma = 0.99
+
+
+#### DISCOUNTED REWARDS #################################
+
+def discount_rewards(r):
+    """ take 1D float array of rewards and compute discounted reward """
+    discounted_r = np.zeros_like(r)
+    running_add = 0
+    for t in reversed(xrange(0, r.size)):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+#### DEFINE AGENT CLASS #################################
+
+class agent():
+    def __init__(self, lr, s_size,a_size,h_size):
+        #These lines established the feed-forward part of the network. The agent takes a state and produces an action.
+        self.state_in= tf.placeholder(shape=[None,s_size],dtype=tf.float32)
+        hidden = slim.fully_connected(self.state_in,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
+        self.output = slim.fully_connected(hidden,a_size,activation_fn=tf.nn.softmax,biases_initializer=None)
+        self.chosen_action = tf.argmax(self.output,1)
+
+        #The next six lines establish the training proceedure. We feed the reward and chosen action into the network
+        #to compute the loss, and use it to update the network.
+        self.reward_holder = tf.placeholder(shape=[None],dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[None],dtype=tf.int32)
+        
+        self.indexes = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
+        self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+
+        self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs)*self.reward_holder)
+        
+        tvars = tf.trainable_variables()
+        self.gradient_holders = []
+        for idx,var in enumerate(tvars):
+            placeholder = tf.placeholder(tf.float32,name=str(idx)+'_holder')
+            self.gradient_holders.append(placeholder)
+        
+        self.gradients = tf.gradients(self.loss,tvars)
+        
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+        self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders,tvars))
+
+
+
+
+
+
+
+
+
+
